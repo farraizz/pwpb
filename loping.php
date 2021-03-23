a70974ab27332dea266b1d6560b9dfb86176f544
Faradina Aiziz Wibisono <68452025+lynarra@users.noreply.github.com>
2021-03-23 22:15:48 +0700

    tugas looping
    tugas looping faradina

---
+++ loping.php
@@ -0,0 +1,26 @@
+
+<?php
+//looping pertama//
+$x = 2;
+
+while($x <= 20) {
+  echo "$x - I love coding <br/>";
+  $x+=2;
+}
+?>
+<html>
+<br>
+</html>
+<?php
+//looping kedua//
+$y = 20;
+
+while($y >= 2) {
+  echo "$y - I will become a mobile developer <br/>";
+  $y-=2;
+}
+?>
+
+
+
+
